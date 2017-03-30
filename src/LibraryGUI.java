830b4e5cb66de7938b0bde5909b8e48f97e9281c
jackdist <jackd083@gmail.com>
2017-03-29 21:21:53 -0400

    added string

--- src/LibraryGUI.java
+++ src/LibraryGUI.java
@@ -33,7 +33,7 @@ public class LibraryGUI extends Application
         // Create the top-level container (main frame) and add contents to it.
         MainStageContainer.setStage(primaryStage, "EOP Login");
         mainStage = MainStageContainer.getInstance();
-        com.apple.eawt.Application.getApplication().setDockIconImage(new ImageIcon("resources/images/shield.png").getImage());
+        //com.apple.eawt.Application.getApplication().setDockIconImage(new ImageIcon("resources/images/shield.png").getImage());
         // Finish setting up the stage (ENABLE THE GUI TO BE CLOSED USING THE TOP RIGHT
         // 'X' IN THE WINDOW), and show it.
         mainStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
