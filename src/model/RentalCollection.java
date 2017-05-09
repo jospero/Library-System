0bf627ccf181667c5c49ec2f730ee87529a158f9
Samuel Babalola <sammytech.net@gmail.com>
2017-05-09 15:56:22 -0400

    My stuff

---
+++ src/model/RentalCollection.java
@@ -0,0 +1,7 @@
+package model;
+
+/**
+ * Created by Sammytech on 5/9/17.
+ */
+public class RentalCollection {
+}
---
+++ src/userinterface/book/RentalCollectionView.java
@@ -0,0 +1,7 @@
+package userinterface.book;
+
+/**
+ * Created by Sammytech on 5/9/17.
+ */
+public class RentalCollectionView {
+}
